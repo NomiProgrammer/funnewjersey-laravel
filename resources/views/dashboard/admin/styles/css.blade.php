12ca8477eefb8975d8b278adae535669f9f3bcb5
isanaullah <sonoftanveer@gmail.com>
2025-06-21 19:45:34 +0500

    Admin Panel Files Arrangments + setup

--- resources/views/dashboard/admin/styles/css.blade.php
+++ resources/views/dashboard/admin/styles/css.blade.php
@@ -1,18 +1,19 @@
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('dashboard/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{asset('dasboard/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="{{asset('dasboard/https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css')}}">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="{{asset('dashboard/css/tempusdominus-bootstrap-4.min.css')}}">
+  <link rel="stylesheet" href="{{asset('dasboard/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
   <!-- iCheck -->
-  <link rel="stylesheet" href="{{asset('dashboard/css/icheck-bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('dasboard/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="{{asset('dashboard/css/jqvmap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('dasboard/plugins/jqvmap/jqvmap.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('dashboard/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{asset('dasboard/dist/css/adminlte.min.css')}}">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{asset('dashboard/css/OverlayScrollbars.min.css')}}">
+  <link rel="stylesheet" href="{{asset('dasboard/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{asset('dashboard/css/daterangepicker.css')}}">
+  <link rel="stylesheet" href="{{asset('dasboard/plugins/daterangepicker/daterangepicker.css')}}">
   <!-- summernote -->
-  <link rel="stylesheet" href="{{asset('dashboard/css/summernote-bs4.min.css')}}">
+  <link rel="stylesheet" href="{{asset('dasboard/plugins/summernote/summernote-bs4.min.css')}}">
--- resources/views/dashboard/admin/styles/js.blade.php
+++ resources/views/dashboard/admin/styles/js.blade.php
@@ -1,34 +1,33 @@
-<!-- jQuery -->
-<script src="{{asset('dashboard/js/jquery.min.js"')}}></script>
+<script src="{{asset('dashboard/plugins/jquery/jquery.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="{{asset('dashboard/js/jquery-ui.min.js"')}}></script>
+<script src="{{asset('dashboard/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="{{asset('dashboard/js/bootstrap.bundle.min.js"')}}></script>
+<script src="{{asset('dashboard/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- ChartJS -->
-<script src="{{asset('dashboard/js/Chart.min.js"')}}></script>
+<script src="{{asset('dashboard/plugins/chart.js/Chart.min.js')}}"></script>
 <!-- Sparkline -->
-<script src="{{asset('dashboard/js/sparkline.js"')}}></script>
+<script src="{{asset('dashboard/plugins/sparklines/sparkline.js')}}"></script>
 <!-- JQVMap -->
-<script src="{{asset('dashboard/js/jquery.vmap.min.js"')}}></script>
-<script src="{{asset('dashboard/js/maps/jquery.vmap.usa.js"')}}></script>
+<script src="{{asset('dashboard/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{asset('dashboard/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
 <!-- jQuery Knob Chart -->
-<script src="{{asset('dashboard/js/jquery.knob.min.js"')}}></script>
+<script src="{{asset('dashboard/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
 <!-- daterangepicker -->
-<script src="{{asset('dashboard/js/moment.min.js"')}}></script>
-<script src="{{asset('dashboard/js/daterangepicker.js"')}}></script>
+<script src="{{asset('dashboard/plugins/moment/moment.min.js')}}"></script>
+<script src="{{asset('dashboard/plugins/daterangepicker/daterangepicker.js')}}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{asset('dashboard/js/tempusdominus-bootstrap-4.min.js"')}}></script>
+<script src="{{asset('dashboard/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
 <!-- Summernote -->
-<script src="{{asset('dashboard/js/summernote-bs4.min.js"')}}></script>
+<script src="{{asset('dashboard/plugins/summernote/summernote-bs4.min.js')}}"></script>
 <!-- overlayScrollbars -->
-<script src="{{asset('dashboard/js/jquery.overlayScrollbars.min.js"')}}></script>
+<script src="{{asset('dashboard/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset('dashboard/js/adminlte.js"')}}></script>
+<script src="{{asset('dashboard/dist/js/adminlte.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{asset('dashboard/js/demo.js"')}}></script>
+<script src="{{asset('dashboard/dist/js/demo.js')}}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{asset('dashboard/js/pages/dashboard.js"')}}></script>
+<script src="{{asset('dashboard/dist/js/pages/dashboard.js')}}"></script>
