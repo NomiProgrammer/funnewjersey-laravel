c9581c5c8b7aabdfcefc19d8e13cee7127b71acd
isanaullah <sonoftanveer@gmail.com>
2025-06-23 19:22:18 +0500

    Admin Panel SideBar

--- resources/views/dashboard/admin/includes/sidebar.blade.php
+++ resources/views/dashboard/admin/includes/sidebar.blade.php
@@ -67,18 +67,6 @@
                   <p>Simple Tables</p>
                 </a>
               </li>
-              <li class="nav-item">
-                <a href="pages/tables/data.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>DataTables</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/tables/jsgrid.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>jsGrid</p>
-                </a>
-              </li>
             </ul>
           </li>
           <li class="nav-header">LABELS</li>
