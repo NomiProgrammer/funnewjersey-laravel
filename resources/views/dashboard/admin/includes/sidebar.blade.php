558557f495a1a0ad22b9bfafe446b60788544d3b
isanaullah <sonoftanveer@gmail.com>
2025-06-28 14:51:14 +0500

    Side bar Settings

--- resources/views/dashboard/admin/includes/sidebar.blade.php
+++ resources/views/dashboard/admin/includes/sidebar.blade.php
@@ -47,42 +47,19 @@
                           <i class="nav-icon fas fa-tachometer-alt"></i>
                           <p>
                               Dashboard
-                              <i class="right fas fa-angle-left"></i>
                           </p>
                       </a>
-                      <ul class="nav nav-treeview">
-                          <li class="nav-item">
-                              <a href="./index.html" class="nav-link active">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>Dashboard v1</p>
-                              </a>
-                          </li>
-                      </ul>
                   </li>
+<!-- Themes -->
                   <li class="nav-item">
                       <a href="#" class="nav-link">
-                          <i class="nav-icon fas fa-table"></i>
+                            <i class="nav-icon fas fa-paint-brush"></i>
                           <p>
-                              Tables
-                              <i class="fas fa-angle-left right"></i>
+                              Themes
                           </p>
                       </a>
-                      <ul class="nav nav-treeview">
-                          <li class="nav-item">
-                              <a href="pages/tables/simple.html" class="nav-link">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>Simple Tables</p>
-                              </a>
-                          </li>
-                      </ul>
                   </li>
-                  <li class="nav-header">LABELS</li>
-                  <li class="nav-item">
-                      <a href="#" class="nav-link">
-                          <i class="nav-icon far fa-circle text-danger"></i>
-                          <p class="text">Important</p>
-                      </a>
-                  </li>
+<!-- Themes -->
 <!-- Parallax Slider -->
                   <li class="nav-item">
                       <a href="#" class="nav-link">
