146e990efe25b4c238089345b23a600d9acb36aa
isanaullah <sonoftanveer@gmail.com>
2025-06-28 15:43:49 +0500

    Files Setup

--- app/Http/Controllers/Admin/ParallaxController.php
+++ app/Http/Controllers/Admin/ParallaxController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Parallax;
 class ParallaxController extends Controller
 {
 public function index(Request $request)
