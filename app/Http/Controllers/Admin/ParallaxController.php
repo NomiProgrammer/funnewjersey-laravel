ad3ac74cca59eab9a11d4b6e5cfc2c824bc5e4b0
isanaullah <sonoftanveer@gmail.com>
2025-06-28 16:03:24 +0500

    Files Setup

--- app/Http/Controllers/Admin/ParallaxController.php
+++ app/Http/Controllers/Admin/ParallaxController.php
@@ -10,7 +10,25 @@ class ParallaxController extends Controller
 public function index(Request $request)
 {
     if ($request->ajax()) {
-$data = Parallax::select('*'); dd($data->first());
+        $data = Parallax::select([
+            'id',
+            'slide_order',
+            'featured_img',
+            'title',
+            'description',
+            'created_by',
+            'create_time',
+            'status',
+            'category',
+            'link',
+            'alttag',
+            'button',
+            'expires',
+            'starts',
+            'city',
+            'county',
+        ]);
+
         return DataTables::of($data)->make(true);
     }
 
