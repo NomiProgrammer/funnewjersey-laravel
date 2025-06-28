5c34e942a5fb6046df1333376bd1480612a2a632
NomiProgrammer <nomaanashiq@gmail.com>
2025-06-28 16:24:03 +0500

    parallax slider admin manage fixed

--- app/Http/Controllers/Admin/ParallaxController.php
+++ app/Http/Controllers/Admin/ParallaxController.php
@@ -5,36 +5,37 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Parallax;
+use Yajra\DataTables\DataTables;
 use Illuminate\Support\Facades\Log;
+
 class ParallaxController extends Controller
 {
-public function index(Request $request)
-{
+    public function index(Request $request)
+    {
 
-    if ($request->ajax()) {
-        dd("ASdfd");
-        $data = Parallax::select([
-            'id',
-            'slide_order',
-            'featured_img',
-            'title',
-            'description',
-            'created_by',
-            'create_time',
-            'status',
-            'category',
-            'link',
-            'alttag',
-            'button',
-            'expires',
-            'starts',
-            'city',
-            'county',
-        ]);
-        return DataTables::of($data)->make(true);
+        if ($request->ajax()) {
+            // dd("ASdfd");
+            $data = Parallax::select([
+                'id',
+                'slide_order',
+                'featured_img',
+                'title',
+                'description',
+                'created_by',
+                'create_time',
+                'status',
+                'category',
+                'link',
+                'alttag',
+                'button',
+                'expires',
+                'starts',
+                'city',
+                'county',
+            ]);
+            return DataTables::of($data)->make(true);
+        }
+
+        return view('dashboard.admin.parallaxslider.index');
     }
-
-    return view('dashboard.admin.parallaxslider.index');
-}
-
 }
--- resources/views/dashboard/admin/parallaxslider/index.blade.php
+++ resources/views/dashboard/admin/parallaxslider/index.blade.php
@@ -1,103 +1,190 @@
 @extends('dashboard.admin.layouts.app')
 @section('page_title', 'Manage Parallax Slides')
 
+
 @section('css')
-    <!-- Same CSS as yours -->
-    <!-- Keep your DataTables, FontAwesome and custom table styling -->
-    <!-- Keep as-is -->
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+    <style>
+        /* Header */
+        table.dataTable thead {
+            background-color: #17a2b8;
+            color: white;
+            font-weight: 600;
+        }
+
+        /* Footer */
+        table.dataTable tfoot {
+            background-color: #17a2b8;
+            color: white;
+            font-weight: 600;
+        }
+
+        /* Sorting arrows on white */
+        table.dataTable thead th.sorting:before,
+        table.dataTable thead th.sorting:after,
+        table.dataTable tfoot th.sorting:before,
+        table.dataTable tfoot th.sorting:after {
+            color: rgba(255, 255, 255, 0.8) !important;
+        }
+
+        .paginate_button.page-item.active a {
+            background-color: #17a2b8 !important;
+            color: white !important;
+        }
+
+        .paginate_button.page-item a:hover {
+            background-color: #e0f7fa !important;
+            color: #000 !important;
+        }
+
+        /* Button styles with icon-colored borders */
+        .dt-buttons .btn-outline-secondary {
+            color: #6c757d !important;
+            border-color: #6c757d !important;
+        }
+
+        .dt-buttons .btn-outline-success {
+            color: #28a745 !important;
+            border-color: #28a745 !important;
+        }
+
+        .dt-buttons .btn-outline-danger {
+            color: #dc3545 !important;
+            border-color: #dc3545 !important;
+        }
+
+        .dt-buttons .btn-outline-primary {
+            color: #007bff !important;
+            border-color: #007bff !important;
+        }
+
+        .dt-buttons .btn {
+            margin-right: 8px;
+            background-color: transparent !important;
+            box-shadow: none !important;
+            padding: 4px 10px !important;
+        }
+
+        .dt-buttons .btn:hover {
+            background-color: transparent !important;
+            opacity: 0.85;
+        }
+    </style>
 @endsection
 
 @section('admin-content')
-@php
-    $pageName = 'Manage Parallax Slides';
-    $pageName2 = 'Parallax Slider Records';
-@endphp
+    @php
+        $pageName = 'Manage Parallax Slides';
+        $pageName2 = 'Parallax Slider Records';
+    @endphp
 
-<div class="content-wrapper">
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>{{ $pageName }}</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">{{ $pageName }}</li>
-                    </ol>
+    <div class="content-wrapper">
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>{{ $pageName }}</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">{{ $pageName }}</li>
+                        </ol>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card card-info">
-                        <div class="card-header">
-                            <h3 class="card-title">{{ $pageName2 }}</h3>
-                            <div class="card-tools">
-                                <a href="" class="float-right btn btn-block btn-success btn-sm">
-                                    <i class="fas fa-plus"></i>&nbsp; Add New
-                                </a>
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card card-info">
+                            <div class="card-header">
+                                <h3 class="card-title">{{ $pageName2 }}</h3>
+                                <div class="card-tools">
+                                    <a href="" class="float-right btn btn-block btn-success btn-sm">
+                                        <i class="fas fa-plus"></i>&nbsp; Add New
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <div class="card-body">
-                            <table id="example2" class="table table-bordered table-striped table-hover">
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Slide Order</th>
-                                        <th>Featured Img</th>
-                                        <th>Title</th>
-                                        <th>Description</th>
-                                        <th>Created By</th>
-                                        <th>Create Time</th>
-                                        <th>Status</th>
-                                        <th>Category</th>
-                                        <th>Link</th>
-                                        <th>Alt Tag</th>
-                                        <th>Button</th>
-                                        <th>Expires</th>
-                                        <th>Starts</th>
-                                        <th>City</th>
-                                        <th>County</th>
-                                    </tr>
-                                </thead>
-                                <tfoot>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Slide Order</th>
-                                        <th>Featured Img</th>
-                                        <th>Title</th>
-                                        <th>Description</th>
-                                        <th>Created By</th>
-                                        <th>Create Time</th>
-                                        <th>Status</th>
-                                        <th>Category</th>
-                                        <th>Link</th>
-                                        <th>Alt Tag</th>
-                                        <th>Button</th>
-                                        <th>Expires</th>
-                                        <th>Starts</th>
-                                        <th>City</th>
-                                        <th>County</th>
-                                    </tr>
-                                </tfoot>
-                                <tbody></tbody>
-                            </table>
+                            <div class="card-body">
+                                <table id="example2" class="table table-bordered table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Slide Order</th>
+                                            <th>Featured Img</th>
+                                            <th>Title</th>
+                                            <th>Description</th>
+                                            <th>Created By</th>
+                                            <th>Create Time</th>
+                                            <th>Status</th>
+                                            <th>Category</th>
+                                            <th>Link</th>
+                                            <th>Alt Tag</th>
+                                            <th>Button</th>
+                                            <th>Expires</th>
+                                            <th>Starts</th>
+                                            <th>City</th>
+                                            <th>County</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Slide Order</th>
+                                            <th>Featured Img</th>
+                                            <th>Title</th>
+                                            <th>Description</th>
+                                            <th>Created By</th>
+                                            <th>Create Time</th>
+                                            <th>Status</th>
+                                            <th>Category</th>
+                                            <th>Link</th>
+                                            <th>Alt Tag</th>
+                                            <th>Button</th>
+                                            <th>Expires</th>
+                                            <th>Starts</th>
+                                            <th>City</th>
+                                            <th>County</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody></tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-</div>
+        </section>
+    </div>
 @endsection
 
 @section('js')
     <!-- Same JS CDN scripts as you have -->
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+
+    <!-- DataTables & Export Plugins -->
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap4.min.js"></script>
+
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.bootstrap4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+
     <script>
         $('#example2').DataTable({
             processing: true,
@@ -105,29 +192,75 @@
             responsive: true,
             autoWidth: false,
             ajax: "{{ route('parallax.index') }}",
-            columns: [
-                { data: 'id', name: 'id' },
-                { data: 'slide_order', name: 'slide_order' },
-                { data: 'featured_img', name: 'featured_img' },
-                { data: 'title', name: 'title' },
-                { data: 'description', name: 'description' },
-                { data: 'created_by', name: 'created_by' },
-                { data: 'create_time', name: 'create_time' },
-                { data: 'status', name: 'status' },
-                { data: 'category', name: 'category' },
-                { data: 'link', name: 'link' },
-                { data: 'alttag', name: 'alttag' },
-                { data: 'button', name: 'button' },
-                { data: 'expires', name: 'expires' },
-                { data: 'starts', name: 'starts' },
-                { data: 'city', name: 'city' },
-                { data: 'county', name: 'county' },
+            columns: [{
+                    data: 'id',
+                    name: 'id'
+                },
+                {
+                    data: 'slide_order',
+                    name: 'slide_order'
+                },
+                {
+                    data: 'featured_img',
+                    name: 'featured_img'
+                },
+                {
+                    data: 'title',
+                    name: 'title'
+                },
+                {
+                    data: 'description',
+                    name: 'description'
+                },
+                {
+                    data: 'created_by',
+                    name: 'created_by'
+                },
+                {
+                    data: 'create_time',
+                    name: 'create_time'
+                },
+                {
+                    data: 'status',
+                    name: 'status'
+                },
+                {
+                    data: 'category',
+                    name: 'category'
+                },
+                {
+                    data: 'link',
+                    name: 'link'
+                },
+                {
+                    data: 'alttag',
+                    name: 'alttag'
+                },
+                {
+                    data: 'button',
+                    name: 'button'
+                },
+                {
+                    data: 'expires',
+                    name: 'expires'
+                },
+                {
+                    data: 'starts',
+                    name: 'starts'
+                },
+                {
+                    data: 'city',
+                    name: 'city'
+                },
+                {
+                    data: 'county',
+                    name: 'county'
+                },
             ],
             dom: "<'d-flex justify-content-between align-items-center mb-3'<'dt-buttons'B><'dataTables_filter'f>>" +
                 "<'table-responsive'tr>" +
                 "<'d-flex justify-content-between align-items-center mt-3'lip>",
-            buttons: [
-                {
+            buttons: [{
                     extend: 'copyHtml5',
                     text: '<i class="fas fa-copy"></i> Copy',
                     className: 'btn btn-outline-secondary btn-sm me-2'
